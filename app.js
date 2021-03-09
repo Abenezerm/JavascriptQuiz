63d40f068d1653d019c0c1085b5487ee987cd6a3
Abenezer Merdassa <abenezermerdassa@gmail.com>
2021-03-09 02:15:04 -0600

    Added some functionality to the js, added questions and some DOM manipulation

--- app.js
+++ app.js
@@ -1,17 +1,95 @@
 const startButton = document.getElementById('start-Button');
 const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('questions');
+const answerButtonsElement = document.getElementById('answer-buttons');
+let randomQuestion;
+let currentQuestion;
+let score = 0;
+startButton.addEventListener('click', startQuiz)
 
-
-
-startButton.addEventListener('click', startGame)
-
-function startGame(){
+function startQuiz(){
   //hides the start button and reveals the hiddent questions when start button is pressed...
   startButton.classList.add('hide')
   questionContainerElement.classList.remove('hide')
+  randomQuestion = questions.sort(() => Math.random() - .5)
+  currentQuestion = 0
   getNextQuestion()
 }
 
 function getNextQuestion(){
-  
+  resetState()
+  showQuestion(randomQuestion[currentQuestion])
 }
+
+function showQuestion(question){
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button  = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct){
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState(){
+  while(answerButtonsElement.firstChild){
+    answerButtonsElement.removeChild
+    (answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e){
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  if (correct){
+    selectedButton.classList.add('correct')
+    score ++
+    console.log(score)
+  }else{
+    selectedButton.classList.add('incorrect')
+  }
+}
+
+function result(element, correct){
+
+}
+
+const questions = [
+  { question:  'What is the HTML tag under which one can write the JavaScript code?',
+    answers: [
+      {text: '<javascript>', correct : false},
+      {text: '<scripted>', correct : false},
+      {text: '<script>', correct : true},
+      {text: '<js>', correct : false}
+    ]
+  },
+  { question:  'Which of the following is the correct syntax to display “GeeksforGeeks” in an alert box using JavaScript?',
+    answers: [
+      {text: 'alertbox(“GeeksforGeeks”);', correct : false},
+      {text: 'msg(“GeeksforGeeks”);', correct : false},
+      {text: 'msgbox(“GeeksforGeeks”);', correct : false},
+      {text: 'alert(“GeeksforGeeks”);', correct : true}
+    ]
+  },
+  { question:  'What is the correct syntax for referring to an external script called “geek.js”?',
+    answers: [
+      {text: '<script src=”geek.js”>', correct : true},
+      {text: '<script href=”geek.js”>', correct : false},
+      {text: '<script ref=”geek.js”>', correct : false},
+      {text: '<script name=”geek.js”>', correct : false}
+    ]
+  },
+  { question:  ' Which of the following is not a reserved word in JavaScript?',
+    answers: [
+      {text: 'interface', correct : false},
+      {text: 'throws', correct : false},
+      {text: 'program', correct : true},
+      {text: ' short', correct : false}
+    ]
+  }
+
+]
